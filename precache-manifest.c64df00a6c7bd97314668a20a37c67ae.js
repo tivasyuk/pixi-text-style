1b4ed158b569cd80c15d917fec9c47bb4e864622
TIvasyuk <tanyaiv494@gmail.com>
2020-06-12 11:53:54 +0300

    Updates

---
+++ precache-manifest.c64df00a6c7bd97314668a20a37c67ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68bfb702eb4550c032a657395c919361",
+    "url": "/pixi-text-style/index.html"
+  },
+  {
+    "revision": "da2520a1b0a041a7bf48",
+    "url": "/pixi-text-style/static/css/main.69f42db8.chunk.css"
+  },
+  {
+    "revision": "bbd4be0154471470f249",
+    "url": "/pixi-text-style/static/js/2.040ccbe9.chunk.js"
+  },
+  {
+    "revision": "f1dfa1256c20464b1eaf3b9c19ffce7b",
+    "url": "/pixi-text-style/static/js/2.040ccbe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da2520a1b0a041a7bf48",
+    "url": "/pixi-text-style/static/js/main.45cac188.chunk.js"
+  },
+  {
+    "revision": "4897cfc91f629f7c3c92",
+    "url": "/pixi-text-style/static/js/runtime-main.0d2e34d0.js"
+  }
+]);
